a7bdaa9538834964a4d4f28b5262738f824e119e
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-10-28 18:22:55 +0300

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by Кирилл on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
